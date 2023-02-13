1b0efec932fec2cda206bd85447ad2e09318b183
the director <the director@DESKTOP-ICVF1BM>
2023-02-13 15:39:57 +0530

    Add project files.

---
+++ BulkyBook.Models/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Range(1,10000)]
+        public double ListPrice { get; set; } //List price of the book
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; } //Final price for one single book
+        [Required]
+        [Range(1, 10000)]
+        public double Price50 { get; set; } //Bulk order price for 50 books
+        [Required]
+        [Range(1, 10000)]
+        public double Price100 { get; set; } //Bulk order price for 100 books
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; } //Marked as a FK using Data Annotations
+        public Category Category { get; set; }
+
+        [Required]
+        public int CoverTypeId { get; set; } //EF core will automatically mark it as a FK
+        public CoverType CoverType { get; set; }
+
+    }
+}
---
+++ BulkyBookWebApp/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,119 @@
+﻿using BulkyBook.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+            };
+
+            if (id == null || id == 0)
+            {
+                //Create Product
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                return View(productVM);
+            }
+            else
+            {
+                //Update Product
+            }
+            return View(productVM);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM obj, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                //string wwwRootPath = _webHostEnvironment.WebRootPath;
+                //if(file != null)
+                //{
+                //    string fileName = Guid.NewGuid().ToString();
+                //    var uploads = Path.Combine(wwwRootPath, @"images\products");
+                //    var extension = Path.GetExtension(file.FileName);
+                //    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create)) 
+                //    {
+                //        file.CopyTo(fileStreams);
+                //    }
+                //    obj.Product.ImageUrl = @"images\products\" + fileName + extension;
+                //}
+
+                //_unitOfWork.Product.Add(obj.Product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product added successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (CoverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(CoverTypeFromDbFirst);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType deleted successfully.";
+            return RedirectToAction("Index");
+        }
+    }
+}
